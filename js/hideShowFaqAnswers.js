f9a83c176ecc3643fea06bb5383186a94455d564
Gabriel de Barros Pontes <gabrielpontes98@gmail.com>
2021-03-19 10:44:39 -0300

    Adding functions to FAQ section

--- js/hideShowFaqAnswers.js
+++ js/hideShowFaqAnswers.js
@@ -4,3 +4,15 @@ function showAnswer(idAnswer){
 function hideAnswer(idAnswer){
     document.getElementById(idAnswer).style.display = "none";
 }
+
+function resizeButton(myButton){
+    console.log(myButton);
+    myButton.style = "font-size:45px";
+    myButton.style.cursor = "pointer";
+
+}
+
+function backButton(myButton){
+    console.log(myButton);
+    myButton.style = "font-size:35px";
+}
